0284bdfd06de47f2604e87106266fcea933ab53a
Mathias Falci de Castro <mathias.falci@gmail.com>
2017-05-23 13:05:05 -0300

    Initial Commit

---
+++ ClientHandler/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ClientHandler
+{
+    class Program
+    {
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern IntPtr OpenProcess(ProcessAccessFlags access, bool inheritHandle, int procId);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, uint nSize, out int lpNumberOfBytesWritten);
+
+        [DllImport("kernel32.dll")]
+        public static extern Int32 CloseHandle(IntPtr hProcess);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool ReadProcessMemory(int hProcess, int lpBaseAddress, byte[] lpBuffer, int dwSize, ref int lpNumberOfBytesRead);
+
+        [Flags]
+        public enum ProcessAccessFlags : uint
+        {
+            All = 0x001F0FFF,
+            Terminate = 0x00000001,
+            CreateThread = 0x00000002,
+            VirtualMemoryOperation = 0x00000008,
+            VirtualMemoryRead = 0x00000010,
+            VirtualMemoryWrite = 0x00000020,
+            DuplicateHandle = 0x00000040,
+            CreateProcess = 0x000000080,
+            SetQuota = 0x00000100,
+            SetInformation = 0x00000200,
+            QueryInformation = 0x00000400,
+            QueryLimitedInformation = 0x00001000,
+            Synchronize = 0x00100000
+        }
+
+        public static void WriteMem(Process p, int address, string value)
+        {
+            var hProc = OpenProcess(ProcessAccessFlags.All, false, (int)p.Id);
+            //var val = new byte[] { (byte)v };
+            byte[] val = Encoding.Default.GetBytes(value + "\0");
+
+            var str = System.Text.Encoding.Default.GetString(val);
+
+            int wtf = 0;
+            WriteProcessMemory(hProc, new IntPtr(address), val, (UInt32)val.LongLength, out wtf);
+
+            CloseHandle(hProc);
+        }
+
+        static void Main(string[] args)
+        {
+            //var p = Process.GetProcessesByName("teste").FirstOrDefault();
+            //WriteMem(p, 0x026BB3D8, "123");
+
+        }
+    }
+}
